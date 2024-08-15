b69a54e9e7162fd3199214ebb1e98516a52750dc
Qsen02 <diyangenchev73@gmail.com>
2024-08-15 11:18:28 +0300

    created user service

---
+++ Delicious food restaurant/client/src/api/userService.js
@@ -0,0 +1,19 @@
+import { post } from "./api";
+
+const endpoint = "/users";
+
+export async function register(data) {
+    return await post(`${endpoint}/register`, data);
+}
+
+export async function login(data) {
+    return await post(`${endpoint}/login`, data);
+}
+
+export async function logout() {
+    await get(`${endpoint}/logout`);
+}
+
+export async function getUserById(userId) {
+    return await get(`${endpoint}/${userId}`);
+}
