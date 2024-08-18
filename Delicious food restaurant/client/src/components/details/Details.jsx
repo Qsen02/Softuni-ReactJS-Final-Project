52dd2900d64a16d2141ed3a35e7302a73605ac54
Qsen02 <diyangenchev73@gmail.com>
2024-08-18 15:09:25 +0300

    Details update

--- Delicious food restaurant/client/src/components/details/Details.jsx
+++ Delicious food restaurant/client/src/components/details/Details.jsx
@@ -4,6 +4,7 @@ import { useGetOneDish } from "../../hooks/useDishes";
 import DetailsButtons from "./details-buttons/DetailsButtons";
 
 import styles from "./Details.module.css"
+
 import { useUserContext } from "../../context/UserContext";
 
 export default function Details() {
@@ -12,21 +13,27 @@ export default function Details() {
     }
     const { dishId } = useParams();
     const { user } = useUserContext();
-    const { dish, setDishHandler, owner, isLoading } = useGetOneDish(initialvalues, dishId);
+    const { dish, setDishHandler, owner, isLoading, isFetchFailed } = useGetOneDish(initialvalues, dishId);
 
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.detailsHeader}>
-                <img src={dish.image} alt={dish.title} />
-                <div>
-                    <h2>{dish.title}</h2>
-                    <p>Category: {dish.category}</p>
-                     <DetailsButtons />
+        <>
+            {isLoading && !isFetchFailed? <div className={styles.loadingSpinner}></div> : ""}
+            {isFetchFailed
+                ? <h1 className={styles.message}>Fetch failed please return to catalog.</h1>
+                : <div className={styles.wrapper}>
+                    <div className={styles.detailsHeader}>
+                        <img src={dish.image} alt={dish.title} />
+                        <div>
+                            <h2>{dish.title}</h2>
+                            <p>Category: {dish.category}</p>
+                            <DetailsButtons />
+                        </div>
+                    </div>
+                    <div className={styles.detailsBody}>
+                        <p>{dish.description}</p>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.detailsBody}>
-                <p>{dish.description}</p>
-            </div>
-        </div>
+            }
+        </>
     )
 }
