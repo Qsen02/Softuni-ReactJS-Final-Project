88052387f3e642a9559a33262955be677f0d4401
Qsen02 <diyangenchev73@gmail.com>
2024-08-24 11:01:29 +0300

    Added css to dish likes component

--- Delicious food restaurant/client/src/components/dishLikes/DishLikes.jsx
+++ Delicious food restaurant/client/src/components/dishLikes/DishLikes.jsx
@@ -1,17 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import DishLikesContent from "./dishLikesContent/DishLikesContent";
 
 import { useGetOneDish } from "../../hooks/useDishes";
 
+import styles from "./DishLikes.module.css"
+
 export default function DishLikes() {
     const {dishId}=useParams();
+    const navigate=useNavigate();
     const {dish}=useGetOneDish({likes:[]},dishId);
 
+    function onBack(){
+        navigate(`/catalog/${dishId}`);
+    }
+
     return (
-        <div>
-            <div>
+        <div className={styles.modal}>
+            <div className={styles.wrapper}>
+                <h2>Users list</h2>
                 {dish.likes.map(el=><DishLikesContent key={el._id} username={el.username}/>)}
+                <button onClick={onBack}>Back</button>
             </div>
         </div>
     )
