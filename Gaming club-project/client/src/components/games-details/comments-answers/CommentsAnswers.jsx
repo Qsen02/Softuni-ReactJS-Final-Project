91581959f1bef706b99c2dc3d44e078475d0cb02
Qsen02 <diyangenchev73@gmail.com>
2024-09-03 10:34:07 +0300

    implement css to answers

--- Gaming club-project/client/src/components/games-details/comments-answers/CommentsAnswers.jsx
+++ Gaming club-project/client/src/components/games-details/comments-answers/CommentsAnswers.jsx
@@ -2,36 +2,41 @@ import { useParams } from "react-router-dom"
 import { useGetAllAnswers } from "../../../hooks/useAnswers"
 import CommentsAnswersDetails from "./comments-answers-details/CommentsAnswersDetails";
 
+import styles from "./CommentAnswers.module.css"
+
 export default function CommentsAnswers() {
     const { commentId } = useParams();
-    const { answers, setAnswersHandler, loading,answersTo } = useGetAllAnswers([], commentId);
+    const { answers, setAnswersHandler, loading, answersTo } = useGetAllAnswers([], commentId);
 
     return (
-        <div>
+        <div className={styles.modal}>
             {loading
-                ? <div></div>
+                ? <div className={styles.loadingSpinner}></div>
                 : ""
             }
-            <h2>Answers to {answersTo.username}</h2>
-            <form>
-                <input type="text" name="content" placeholder="Enter answer here" />
-                <button>Answer</button>
-            </form>
-            <div>
-                {answers.length > 0 && !loading
-                    ? answers.map(el => <CommentsAnswersDetails
-                        key={el._id}
-                        id={el._id}
-                        content={el.content}
-                        username={el.username}
-                        ownerId={el.ownerId}
-                    />)
-                    : <h2>No answers yet</h2>
-                }
-                {answers.length == 0 && loading
-                    ? <h2>Loading answers</h2>
-                    : ""
-                }
+            <div className={styles.wrapper}>
+                <h2>Answers to {answersTo.username}</h2>
+                <form>
+                    <input type="text" name="content" placeholder="Enter answer here" />
+                    <button>Answer</button>
+                </form>
+                <div className={styles.answers}>
+                    {answers.length > 0 && !loading
+                        ? answers.map(el => <CommentsAnswersDetails
+                            key={el._id}
+                            id={el._id}
+                            content={el.content}
+                            username={el.username}
+                            ownerId={el.ownerId}
+                        />)
+                        : <h2>No answers yet</h2>
+                    }
+                    {answers.length == 0 && loading
+                        ? <h2>Loading answers</h2>
+                        : ""
+                    }
+                </div>
+
             </div>
         </div>
     )
