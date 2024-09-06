c9710bae4d766484d5bdf10c99d7315ac67b7642
Qsen02 <diyangenchev73@gmail.com>
2024-09-06 11:30:26 +0300

    Implement movie model

--- Movie list/server/src/config/mongoose.js
+++ Movie list/server/src/config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Users } = require("../models/users");
+const { Movies } = require("../models/movies");
 
 async function runDB() {
     await mongoose.connect("mongodb://localhost:27017/Movie-list");
---
+++ Movie list/server/src/models/movies.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    genre: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    },
+    year: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    likes: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        ref: "Users",
+        default: []
+    },
+    saves: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        ref: "Users",
+        default: []
+    },
+    comments: {
+        type: [mongoose.SchemaTypes.ObjectId],
+        ref: "Comments",
+        default: []
+    }
+})
+
+const Movies = mongoose.model("Movies", movieSchema);
+
+module.exports = {
+    Movies
+}
