73e3a4ff6b2309aa9cc7b8ea6aec2d53e0d673ec
Qsen02 <diyangenchev73@gmail.com>
2024-09-06 11:17:14 +0300

    Created mongoose config

--- Movie list/server/src/server.js
+++ Movie list/server/src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { expressConfig } = require("./config/express");
+const { runDB } = require("./config/mongoose");
 
 const app = express();
 
 const port = 3000;
 
 async function start() {
+    await runDB();
+
     expressConfig(app);
 
     app.listen(() => {
