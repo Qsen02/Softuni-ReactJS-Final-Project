3aa9b7be280d1f46312bb2cdb1791d96d5ee654a
Qsen02 <diyangenchev73@gmail.com>
2024-09-07 22:16:12 +0300

    Implement css for home content

--- Movie list/client/src/components/home/Home.tsx
+++ Movie list/client/src/components/home/Home.tsx
@@ -13,12 +13,10 @@ export default function Home() {
             </div>
             <div className={styles.homeBody}>
                 <h2>Top 3 movies</h2>
-                <div>
                     {movies.length > 0
                         ? movies.map(el => <HomeContent key={el._id} id={el._id} title={el.title} genre={el.genre} image={el.image}/>)
                         : <h2>No movies yet</h2>
                     }
-                </div>
             </div>
         </>
     )
