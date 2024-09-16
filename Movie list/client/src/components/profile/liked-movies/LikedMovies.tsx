a00c7d63886f4fd392b188b75311fa16d83ab335
Qsen02 <diyangenchev73@gmail.com>
2024-09-16 10:29:59 +0300

    CSS update on liked movie list and implement backdrop button

--- Movie list/client/src/components/profile/liked-movies/LikedMovies.tsx
+++ Movie list/client/src/components/profile/liked-movies/LikedMovies.tsx
@@ -1,6 +1,7 @@
 import LikedMoviesDetails from "./liked-movies-details/LikedMoviesDetails";
 
 import styles from "../ProfileMovies.module.css"
+import { useNavigate } from "react-router-dom";
 
 type LikedMoviesProps = {
     likedMovies: [
@@ -16,10 +17,17 @@ type LikedMoviesProps = {
 export default function LikedMovies({
     likedMovies
 }: LikedMoviesProps) {
+    const navigate=useNavigate();
+
+    function onBack(){
+        navigate("/profile");
+    }
+
     return (
         <div className={styles.modal}>
+            <button onClick={onBack}>X</button>
+            <h1>Your liked movies</h1>
             <section className={styles.catalogContent}>
-                    <h1>Your liked movies</h1>
                     {likedMovies.length > 0
                         ? likedMovies.map(el => <LikedMoviesDetails key={el._id} id={el._id} title={el.title} image={el.image} genre={el.genre} />)
                         : <h2>No liked movies yet</h2>
