5ac8e1cd63a08b2486e3d87764879a4c8b87ff11
Qsen02 <diyangenchev73@gmail.com>
2024-09-24 10:02:25 +0300

    Implement answers model in server

--- Movie list/server/src/config/mongoose.js
+++ Movie list/server/src/config/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const { Users } = require("../models/users");
 const { Movies } = require("../models/movies");
 const { Comments } = require("../models/comments");
+const { Answers } = require("../models/answer");
+
 async function runDB() {
     await mongoose.connect("mongodb://localhost:27017/Movie-list");
     console.log("Database connected");
--- Movie list/server/src/models/answer.js
+++ Movie list/server/src/models/answer.js
@@ -1,36 +1,27 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
+const answerSchema = new mongoose.Schema({
     username: {
         type: String,
         require: true
     },
     content: {
         type: String,
-        require: true,
+        require: true
     },
     ownerId: {
         type: mongoose.SchemaTypes.ObjectId,
         ref: "Users"
     },
-    movieId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: "Movies"
-    },
     likes: {
         type: [mongoose.SchemaTypes.ObjectId],
         ref: "Users",
         default: []
-    },
-    answers: {
-        type: [mongoose.SchemaTypes.ObjectId],
-        ref: "Answers",
-        default: []
     }
 })
 
-const Comments = mongoose.model("Comments", commentSchema);
+const Answers = mongoose.model("Answers", answerSchema);
 
 module.exports = {
-    Comments
+    Answers
 }
