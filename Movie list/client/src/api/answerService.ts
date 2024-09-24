5a6164efe331d7258cf327c6510edb22df090971
Qsen02 <diyangenchev73@gmail.com>
2024-09-24 16:37:36 +0300

    Implement answer service

---
+++ Movie list/client/src/api/answerService.ts
@@ -0,0 +1,49 @@
+import { del, get, post, put } from "./api";
+
+const endpoint = "asnwers";
+
+type AnswerType = {
+    username: string,
+    content: string,
+    ownerId: string,
+    likes: {}[]
+}
+
+type CommentType = {
+    username: string,
+    content: string,
+    ownerId: string,
+    movieId: string,
+    likes: {}[],
+    answers: AnswerType[]
+}
+
+export async function getAnswerById(answerId: string) {
+    const answer = await get(`${endpoint}/${answerId}`);
+    return answer as AnswerType;
+}
+
+export async function createAnswer(data: {}, commentId: string) {
+    const comment=await post(`${endpoint}/in/comment/${commentId}`,data);
+    return comment as CommentType;
+}
+
+export async function deleteAnswer(answerId:string,commentId:string){
+    const comment=await del(`${endpoint}/${answerId}/in/${commentId}`);
+    return comment as CommentType;
+}
+
+export async function editAnswer(answerId:string,commentId:string,data:{}){
+    const comment=await put(`${endpoint}/${answerId}/in/${commentId}`,data);
+    return comment as CommentType;
+}
+
+export async function likeAnswer(answerId:string,commentId:string){
+    const comment=await post(`${endpoint}/${answerId}/in/${commentId}/like`,{});
+    return comment as CommentType;
+}
+
+export async function unlikeAnswer(answerId:string,commentId:string){
+    const comment=await post(`${endpoint}/${answerId}/in/${commentId}/unlike`,{});
+    return comment as CommentType;
+}
