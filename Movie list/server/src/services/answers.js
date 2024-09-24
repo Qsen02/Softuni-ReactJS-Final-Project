73a6744a6d2a82d2486d90ecf2b3966a0cf89a06
Qsen02 <diyangenchev73@gmail.com>
2024-09-24 10:21:55 +0300

    Implement asnwer service

---
+++ Movie list/server/src/services/answers.js
@@ -0,0 +1,39 @@
+const { Answers } = require("../models/answer");
+const { Comments } = require("../models/comments");
+
+async function createAnswer(data, user, commentId) {
+    const newAnswer = new Answers(data);
+    newAnswer.ownerId = user._id;
+    newAnswer.save();
+    await Comments.findByIdAndUpdate(commentId, { $push: { answers: newAnswer._id } });
+}
+
+async function deleteAnswer(answerId, commentId) {
+    await Answers.findByIdAndDelete(answerId);
+    await Comments.findByIdAndUpdate(commentId, { $pull: { answers: answerId } });
+}
+
+async function editAnswer(data, answerId) {
+    await Answers.findByIdAndUpdate(answerId, { $set: data });
+}
+
+async function likeAnswer(user, answerId) {
+    await Answers.findByIdAndUpdate(answerId, { $push: { likes: user._id } });
+}
+
+async function checkAnswerId(answerId) {
+    const answers = await Answers.find().lean();
+    const isValid = answers.find(el => el._id.toString() == answerId);
+    if (isValid) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = {
+    createAnswer,
+    deleteAnswer,
+    editAnswer,
+    checkAnswerId,
+    likeAnswer
+}
