9bdc81a707298fd7cf310b5d56bcbffee11151b6
Qsen02 <diyangenchev73@gmail.com>
2024-09-30 11:16:27 +0300

    Implement loading spinner in comment likes

--- Movie list/client/src/components/movie-details/comment-likes/CommentLikes.tsx
+++ Movie list/client/src/components/movie-details/comment-likes/CommentLikes.tsx
@@ -6,11 +6,11 @@ import CommentLikesDetails from "./comment-likes-details/CommentLikesDetails";
 import styles from "../movie-details-likes/MovieDetailsLikes.module.css";
 
 export default function CommentLikes() {
-    const {movieId, commentId } = useParams();
-    const { comment } = useGetOneComment({ content: "", username: "", likes: [] }, commentId);
-    const navigate=useNavigate();
+    const { movieId, commentId } = useParams();
+    const { comment, loading, fetchError, setFetchError } = useGetOneComment({ content: "", username: "", likes: [] }, commentId);
+    const navigate = useNavigate();
 
-    function onBack(){
+    function onBack() {
         try {
             navigate(`/catalog/${movieId}`);
         } catch (err) {
@@ -18,24 +18,35 @@ export default function CommentLikes() {
                 navigate("/404");
                 return;
             }
+            setFetchError(true);
             return;
         }
     }
     return (
-        <div className={styles.modal}>
-            <section>
-                <button onClick={onBack}>X</button>
-                <h2>Comment like list</h2>
-                {comment.likes.length > 0
-                    ? comment.likes.map(el => <CommentLikesDetails
-                        key={(el as { _id: string })._id}
-                        userId={(el as { _id: string })._id}
-                        image={(el as { profileImage: string }).profileImage}
-                        username={(el as { username: string }).username}
-                    />)
-                    : <h2>No likes yet</h2>
-                }
-            </section>
-        </div>
+        <>
+            {loading && !fetchError
+                ? <div className={styles.loadingSpinner}></div>
+                : ""
+            }
+            <div className={styles.modal}>
+                <section>
+                    <button onClick={onBack}>X</button>
+                    <h2>Comment like list</h2>
+                    {comment.likes.length == 0 && !loading && !fetchError
+                        ? <h2>No likes yet</h2>
+                        : loading && !fetchError
+                            ? <h2>Likes loading...</h2>
+                            : fetchError
+                                ? <h2>Likes can't be loaded, please try again later.</h2>
+                                : comment.likes.map(el => <CommentLikesDetails
+                                    key={(el as { _id: string })._id}
+                                    userId={(el as { _id: string })._id}
+                                    image={(el as { profileImage: string }).profileImage}
+                                    username={(el as { username: string }).username}
+                                />)
+                    }
+                </section>
+            </div>
+        </>
     )
 }
